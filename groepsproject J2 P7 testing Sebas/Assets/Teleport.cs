ac0bc74623af028e3117bc9be62d7bf6a1e3dde0
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2018-05-22 16:09:34 +0200

    VR implemented (HOPELIJK

---
+++ groepsproject J2 P7 testing Sebas/Assets/Teleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    private Hand leftHand;
+    private Hand rightHand;
+
+	void Start () {
+
+       leftHand = GameObject.Find("Controller(left)").GetComponent<Hand>();
+       rightHand = GameObject.Find("Controller(right)").GetComponent<Hand>();
+
+    }
+	
+	void Update () {
+
+        TeleportPlayer();
+	}
+
+    public void TeleportPlayer()
+    {
+        if (leftHand.controller.GetHairTriggerDown())
+        {
+            print("Works");
+        }
+    }
+}
